2f6f506327535629f59c9bbfecf12887a4371696
TheWhite33 <rohitrc2005@gmail.com>
2026-01-12 21:42:59 +0530

    cron job added

--- server.ts
+++ server.ts
@@ -5,11 +5,13 @@ import { connectDB } from "./src/config/database";
 import authRoutes from './src/modules/auth/auth.route'
 import { errorHandler } from "./src/middlewares/errorHandler";
 import merchRoutes from './src/modules/merch/merch.route'
+import job from "./src/config/cron";
 
 dotenv.config()
 
 const app: Application = express()
 
+job.start()
 app.use(cors());
 app.use(express.json());
 
